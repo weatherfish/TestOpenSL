8a57952ebd1b4ea123331bb7abca4980b32472d6
Felix Wang <felix@aopacloud.sg>
2024-10-26 10:14:31 +0800

    init

--- app/src/main/cpp/utils/log.h
+++ app/src/main/cpp/utils/log.h
@@ -7,7 +7,7 @@
 
 #include <android/log.h>
 
-#define LOGD(FORMAT, ...) __android_log_print(ANDROID_LOG_DEBUG, "opensl", FORMAT)
-#define LOGE(FORMAT, ...) __android_log_print(ANDROID_LOG_ERROR, "opensl", FORMAT)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "opensl", __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "opensl", __VA_ARGS__)
 
 #endif //TESTOPENSL_LOG_H
